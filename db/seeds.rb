cec361f24bc68c5f27a0ac2cb8ca9b8acc547b78
Nattida Wong <nattidac.wong@gmail.com>
2020-12-22 21:31:00 -0500

    Added Seeds

---
+++ db/seeds.rb
@@ -0,0 +1,10 @@
+Coach.create(name: "Gunnar Peterson", gender: "male", age: 58)
+
+Coach.create(name: "Jillian Michaels", gender: "female", age: 46)
+
+Coach.create(name: "Shaun T", gender: "male", age: 42)
+
+Coach.create(name: "Tracy Anderson", gender: "female", age: 45)
+
+Coach.create(name: "Mark Jenkins", gender: "male", age: 77)
+
