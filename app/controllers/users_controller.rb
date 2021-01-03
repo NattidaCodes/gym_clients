2db4fb1e8a0a4c29692f4be8fa244422d1bf94ff
Nattida Wong <nattidac.wong@gmail.com>
2021-01-02 22:01:20 -0500

    user authentication at login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,8 +19,18 @@ class UsersController < ApplicationController
         end
     end
 
-    get "login" do
+    get "/login" do
         erb :"users/login"
+    end
+
+    post "/login" do
+        user = User.find_by(username:params[:user][:username])
+        if user && user.authenticate(params[:user][:password])
+            session[:user_id] = user.id
+            redirect "/clients"
+        else
+            redirect "/login"
+        end
     end
 
     get "/logout" do
